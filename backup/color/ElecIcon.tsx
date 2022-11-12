f1aa80dda30f7b2cad7bee459f9e4e7e97d8b918
Griever <thrashanddestroy@gmail.com>
2022-11-12 19:16:38 +0100

    rm type

---
+++ backup/color/ElecIcon.tsx
@@ -0,0 +1,14 @@
+import { SVGProps } from 'react';
+const ElecIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <g fill="none">
+      <circle cx={16} cy={16} r={16} fill="#F90" />
+      <g fill="#FFF">
+        <path d="m10.76 27.587 12.666-13.303H15.76z" />
+        <path d="M8 18.27h7.666l7.76-3.986H15.76z" />
+        <path d="M19.51 4 8 18.27h7.666z" />
+      </g>
+    </g>
+  </svg>
+);
+export default ElecIcon;
---
+++ src/components/Svg/Color/index.ts
@@ -0,0 +1,1 @@
+export { default as AaveIcon } from './AaveIcon';
